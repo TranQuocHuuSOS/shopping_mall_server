5b500c11cf320948b54f4b29f736797ef12e0ecc
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2024-08-19 15:53:30 +0700

    edit new port in .env file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.use("/auth", authRouter);
 app.use("/product", productRouter);
 app.use("/booking", bookingRouter);
